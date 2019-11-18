2750ea67be319818127af887d8db081c28ef66a1
=James Sellman <=james.b.sellman@googlemail.com>
2019-11-18 19:33:20 +0000

    lessons 2.11 through to 2.14

--- Udemy Section2.py
+++ Udemy Section2.py
@@ -8,3 +8,17 @@ print(temp)
 
 b=temp.split(' ')[2]
 print(b)
+
+a= ["b",'c','d']
+a1=a[2]
+print(a1)
+
+
+
+print( "I struggled with Question "+["b",'c','d'][1])
+
+#Library practice
+
+lib1= {"name":"James","age": 23,'hobby':"python"}
+
+print(lib1["James"])
